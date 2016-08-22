1a9b37c6d10d47c649fea2b4ac098c63a7a85803
shinya takahashi <s.takahashi313@gmail.com>
2016-08-22 22:09:51 +0900

    Added dependencies

--- lib/normalizr_ruby.rb
+++ lib/normalizr_ruby.rb
@@ -1,5 +1,10 @@
-require 'active_support/core_ext/class/attribute'
-require "normalizr_ruby/version"
+require "active_support/core_ext/class/attribute"
+require "active_support/core_ext/hash/except"
+require "active_support/core_ext/hash/keys"
+require "active_support/core_ext/object/blank"
+require "active_support/configurable"
+require "active_support/concern"
+
 
 module NormalizrRuby
   include ActiveSupport::Configurable
@@ -7,3 +12,9 @@ module NormalizrRuby
     :camel_lower
   end
 end
+
+require "action_controller/normalizable"
+require "normalizr_ruby/version"
+require "normalizr_ruby/key_transform"
+require "normalizr_ruby/schema"
+require "normalizr_ruby/converter"
